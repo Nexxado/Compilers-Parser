a66377f6ca0888d6eecf13d3acddbdb9463199e7
Netanel Draiman <ndraiman@gmail.com>
2016-05-13 12:22:57 +0300

    commented code

--- src/syntax/SyntaxMain.java
+++ src/syntax/SyntaxMain.java
@@ -41,6 +41,7 @@ public class SyntaxMain {
 		if(parse_table == null)
 			return;
 		
+		//Initialize Stack
 		int graphNodeId = 0;
 		Stack<String> parse_stack = new Stack<String>();
 		parse_stack.push(END_OF_INPUT_ID);
@@ -57,42 +58,48 @@ public class SyntaxMain {
 		TokenInfo token;
 		
 		StringBuilder builder = new StringBuilder();
-		builder.append("digraph G {" + System.lineSeparator()); //System.lineSeparator() = get system-dependant new line char
+		builder.append("digraph G {" + System.lineSeparator()); //System.lineSeparator() = get system-dependent new line char
 		
 		try {
 			
 			sc = new Scanner(inputFilename);
-			outputFilename = inputFilename.substring(0, inputFilename.lastIndexOf('.')) + ".ptree";
-			outputFile = new File(outputFilename);
-			writer = new FileWriter(outputFile);
 			
 			do {
 
+				//Get next token
 				token = sc.yylex();
 				String tokenTypeString = token.getType().toString();
 				System.out.println("token: " + token); //TODO DEBUG
 				
-				if(token.getType() == TokenTypeEnum.WHITE || token.getType() == TokenTypeEnum.CMMNT) //Ignore whitespace and comments
+				//Ignore whitespace and comments
+				if(token.getType() == TokenTypeEnum.WHITE || token.getType() == TokenTypeEnum.CMMNT) 
 					continue;
 				
-				if(!terminals.contains(tokenTypeString)) {
-					writer.close();
+				//Check if token type isn't part of LL(1) table
+				if(!terminals.contains(tokenTypeString))
 					throw new LL1Exception();
-				}
 				
 				String check = parse_stack.pop();
 				String check_noid = check.substring(0, check.indexOf('_'));
 				System.out.println("check 1: " + check); //TODO DEBUG
 				
+				//While stack top is non_terminal
 				while(non_terminals.contains(check_noid)) {
-					String[] tab = parse_table.get(check_noid).get(terminals.get(terminals.indexOf(tokenTypeString))).split(";");
-					System.out.println("tab " + Arrays.toString(tab)); //TODO DEBUG
+					String tab = parse_table.get(check_noid).get(terminals.get(terminals.indexOf(tokenTypeString)));
+					
+					//Check if empty tab
+					if(tab == null || tab.isEmpty() || tab.equals(""))
+						throw new LL1Exception();
+					
+					String[] tab_tokens = tab.split(";");
+					System.out.println("tab " + Arrays.toString(tab_tokens)); //TODO DEBUG
 					
 					ArrayList<GraphNode> parse_sub_tree = new ArrayList<GraphNode>();
 					
-					for(int i = tab.length - 1; i >= 0; i--) { //Insert tab into stack in reverse order
-						parse_stack.push(tab[i] + "_" + graphNodeId);
-						parse_sub_tree.add(new GraphNode(check, tab[i] + "_" + graphNodeId++));
+					//Insert Grammar rule to stack and graph (in reverse order)
+					for(int i = tab_tokens.length - 1; i >= 0; i--) {
+						parse_stack.push(tab_tokens[i] + "_" + graphNodeId);
+						parse_sub_tree.add(new GraphNode(check, tab_tokens[i] + "_" + graphNodeId++));
 					}
 					
 					Collections.reverse(parse_sub_tree);
@@ -100,16 +107,12 @@ public class SyntaxMain {
 						builder.append(parse_sub_tree.get(i) + System.lineSeparator());
 					}
 					
-//					graphNodeId -= tab.length;
-//					for(int i = 0; i < tab.length; i++)
-//						builder.append(new GraphNode(check, tab[i] + "_" + graphNodeId++)  + System.lineSeparator());
-					
 					System.out.println("stack: " + parse_stack); //TODO DEBUG
 					System.out.println("******* \ngraph: " + builder.toString() + "********"); //TODO DEBUG
 					check = parse_stack.pop();
 					check_noid = check.substring(0, check.indexOf('_'));
 					
-					//If Epslion, discard it and continue
+					//If Epsilon, discard it and continue
 					if(check_noid.equals(EPSILON_ID)) {
 						check = parse_stack.pop();
 						check_noid = check.substring(0, check.indexOf('_'));
@@ -117,30 +120,31 @@ public class SyntaxMain {
 					}
 				}
 				
-				
-				
 				System.out.println("check 2: " + check); //TODO DEBUG
 				System.out.println("stack: " + parse_stack); //TODO DEBUG
 				
-				if(!check_noid.equals(tokenTypeString)) {
-					writer.close();
+				//Terminal from stack isn't equal to Terminal from input
+				if(!check_noid.equals(tokenTypeString))
 					throw new LL1Exception();
-				}
 				
-				if(token.getType() == TokenTypeEnum.ID)
+				// Insert ID attributes to graph
+				if(token.getType() == TokenTypeEnum.ID) 
 					builder.append(new GraphNode(check, token.getAttribute() + "_" + graphNodeId++)  + System.lineSeparator());
 
 			} while(token.getType() != TokenTypeEnum.EOF);
 			
 			
 			//FIXME depend on EOF token or on "$"?
-			if(parse_stack.empty() || !parse_stack.peek().equals(END_OF_INPUT_ID)) {
-				writer.close();
+			if(parse_stack.empty() || !parse_stack.peek().equals(END_OF_INPUT_ID))
 				throw new LL1Exception();
-			}
 
 			builder.append("}");
 			
+			//Write graph string to output file
+			outputFilename = inputFilename.substring(0, inputFilename.lastIndexOf('.')) + ".ptree";
+			outputFile = new File(outputFilename);
+			
+			writer = new FileWriter(outputFile);
 			writer.write(builder.toString());
 			writer.close();
 			
@@ -160,6 +164,11 @@ public class SyntaxMain {
 
 	
 	
+	
+	/**
+	 * @param configFilename : Name of config file describing LL(1) table.
+	 * @return HashMap of HashMaps defining each tab in the LL(1) table.
+	 */
 	private static HashMap<String, HashMap<String, String>> parseConfigFile(String configFilename) {
 		
 		System.out.println("Parsing Config File"); //TODO DEBUG
@@ -188,8 +197,9 @@ public class SyntaxMain {
 			while((line = reader.readLine()) != null) {
 				
 //				System.out.println("line read: " + line); //TODO DEBUG
+				//Ignore comments in config file
 				if(line.charAt(0) == '#')
-					continue; //ignore comments in config file
+					continue; 
 				
 				String non_terminal = line.substring(0, line.indexOf('='));
 //				System.out.println("non_terminal: " + non_terminal); //TODO DEBUG
@@ -209,8 +219,7 @@ public class SyntaxMain {
 				
 				table.put(non_terminal, map);
 			}
-			
-			
+
 			reader.close();
 			
 		} catch (FileNotFoundException e) {
